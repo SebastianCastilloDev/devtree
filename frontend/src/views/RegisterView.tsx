3f20976b568da8a43207911de45824f35c3973f8
Sebastian Castillo <tantrum5535@gmail.com>
2024-11-25 20:21:37 -0300

    cargando imagenes

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,6 +1,13 @@
-
+import { Link } from "react-router-dom"
 export default function RegisterView() {
   return (
-    <div>RegisterView</div>
+    <>
+      <div>RegisterView</div>
+      <nav>
+        <Link to="/auth/login">
+          ¿Ya tienes una cuenta? Inicia Sesión
+        </Link>
+      </nav>
+    </>
   )
 }
