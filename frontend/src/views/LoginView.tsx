d9b80f8b446a748d433f7680c3263ae62b41bd99
Sebastian Castillo <tantrum5535@gmail.com>
2024-11-25 20:31:06 -0300

    Outlet para manejar layouts

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,14 +3,6 @@ import { Link } from "react-router-dom"
 export default function LoginView() {
   return (
     <>
-      <div className="bg-slate-800 min-h-screen">
-        <div className="max-w-lg mx-auto pt-10 px-5">
-          <img src="/logo.svg" alt="logo devtree" />
-          <div className="py-10">
-            <h1 className="text-white">Login</h1>
-          </div>
-        </div>
-      </div>
       <nav>
         <Link to="/auth/register">
           ¿No tienes una cuenta? Crea una aquí
