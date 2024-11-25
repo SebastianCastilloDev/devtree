3ea4a7dfb45301aefd975eac9fe6cde1e475d751
Sebastian Castillo <tantrum5535@gmail.com>
2024-11-25 20:36:19 -0300

    manejando la navegacion con layouts

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom"
 export default function LoginView() {
   return (
     <>
-      <nav>
-        <Link to="/auth/register">
+      <h1 className="text-4xl text-white font-bold">Iniciar sesión</h1>
+
+      <nav className="mt-10">
+        <Link to="/auth/register" className="text-center text-white text-lg block">
           ¿No tienes una cuenta? Crea una aquí
         </Link>
       </nav>
