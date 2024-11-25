171bbed6eee1a7c2fd7914474ea574aa52a6209e
Sebastian Castillo <tantrum5535@gmail.com>
2024-11-25 04:55:21 -0300

    revisar si el usuario existe

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -48,6 +48,16 @@ export const login = async (req: Request, res: Response) => {
     return res.status(400).json({errors: errors.array()})
   }
 
+  const {email, password} = req.body
+
+  const user = await User.findOne({email})
+  if (!user) {
+    const error = new Error('El usuario no existe')
+    return res.status(404).json({error: error.message})
+  } 
+
+  // Comprobar el password
+  
   
 
 
