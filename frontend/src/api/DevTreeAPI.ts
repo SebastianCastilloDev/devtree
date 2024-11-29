ee0ee6a45d7cfe1abdd40220cf8eac2a6753e7a9
Sebastian Castillo <tantrum5535@gmail.com>
2024-11-28 22:20:24 -0300

    devolviendo al usuario

--- frontend/src/api/DevTreeAPI.ts
+++ frontend/src/api/DevTreeAPI.ts
@@ -3,10 +3,17 @@ import api from "../config/axios"
 import { toast } from "sonner"
 
 export async function getUser() {
+
+  const token = localStorage.getItem('AUTH_TOKEN')
+  
   try {
 
-    const {data} = await api('/user')
-    console.log(data)
+    const {data} = await api('/user', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    return data
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       toast.error(error.response.data.error)
